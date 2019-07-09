f4c3dbb7be9dca210cf795f666caec3fabcc1981
ShiniYanada <ysthon@gmail.com>
2019-07-09 18:03:02 +0900

    install axios and element-ui , setup element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-form label-position="right" label-width="100px" :model="user">
+      <el-form-item label="Email">
+        <el-input v-model="user.email"></el-input>
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input v-model="user.password"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary" plain @click="login">Login</el-button>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+      }
+    };
+  },
+  created(){
+    
+  },
+  methods: {
+    login(){
+      
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
