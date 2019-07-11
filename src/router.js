4098bd05bc037ca2f489fcf8fd17d4a667e05d5c
ShiniYanada <ysthon@gmail.com>
2019-07-11 18:06:49 +0900

    認証に関するナビゲーションガードを'/','/login'に追加

--- src/router.js
+++ src/router.js
@@ -1,17 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const ifNotAuthenticated = (to, from, next) => {
+  if(!store.getters['auth/isAuthenticated']){
+    next()
+    return
+  }
+  next('/')
+}
+
+const ifAuthenticated = (to, from , next) => {
+  if(store.getters['auth/isAuthenticated']){
+    next()
+    return 
+  }
+  next('/login')
+}
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      beforeEnter: ifAuthenticated
     },
     {
       path: '/login',
@@ -19,7 +37,10 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "login" */ './views/Login.vue')
+      component: () => import(/* webpackChunkName: "login" */ './views/Login.vue'),
+      beforeEnter: ifNotAuthenticated
     }
   ]
 })
+
+export default router
