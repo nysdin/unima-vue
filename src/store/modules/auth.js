9ca2685caca1f62fdd11a13823b79c714f2e85ab
ShiniYanada <ysthon@gmail.com>
2019-07-11 10:58:21 +0900

    認証apiに関する通信状態（loading, succes, error)を管理するmutationsを追加しlogin()に処理を追加

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -7,31 +7,51 @@ const auth = {
         status: '',
     },
     getters: {
+        //ログインの真偽
         isAuthenticated(state){
             return !!state.token
         },
-        //APIの状態、loading or success or error
+        //authenticationに関するAPIの状態、loading or success or error
         authStatus(state){
             return state.status
         }
     },
     mutations: {
-
+        //認証apiの通信中
+        authRequest(state){
+            state.status = 'loading'
+        },
+        //認証apiの成功
+        authSuccess(state){
+            state.status = 'success'
+        },
+        //認証apiの失敗
+        authError(state){
+            state.status = 'error'
+        }
     },
     actions: {
         login({ commit }, options){
             return new Promise( (resolve, reject) => {
+                //api通信中に設定
+                commit('authRequest')
+                //認証api通信
                 request.post(options.url, options)
                     .then( response => {
-                        console.log(response.data.data)
+                        //返ってきたuser情報をstoreにセットするcommit
                         commit('user/setUser', response.data.data, { root: true })
                         const headers = response.headers
+                        //localStorageにheadersの情報をセット
                         localStorage.setItem('access-token', headers['access-token'])
                         localStorage.setItem('client', headers['client'])
                         localStorage.setItem('uid', headers['uid'])
+                        //認証apiの成功
+                        commit('authSuccess')
                         resolve()
                     })
                     .catch( error => {
+                        //認証apiの失敗
+                        commit('authError')
                         console.log(error)
                         reject()
                     })
