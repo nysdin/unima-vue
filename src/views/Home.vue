3c02cb46a3d3375aa30075b89b3b1db20e20b169
ShiniYanada <ysthon@gmail.com>
2019-07-14 21:57:18 +0900

    商品一覧を表示させる処理を簡易的に追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,20 @@
     <h1>Unima</h1>
     <router-link to='/sell'>出品</router-link>
     <el-button type="primary" plain @click="logout">Logout</el-button>
+    <el-row :gutter="20">
+      <el-col :span="6" v-for="item in items" :key="item.id">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+          <div style="padding: 14px;">
+            <span>{{ item.name }}</span>
+            <div class="bottom clearfix">
+              <p>{{ item.description }}</p>
+              <p>{{ item.price }}</p>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -11,6 +25,11 @@
 
 export default {
   name: 'home',
+  computed: {
+    items(){
+      return this.$store.state.product.products
+    }
+  },
   methods: {
     logout(){
       this.$store.dispatch('auth/logout', { auth: true, url: 'http://localhost:3000/api/v1/auth/sign_out' })
@@ -25,3 +44,30 @@ export default {
   }
 }
 </script>
+
+<style>
+  .el-row {
+    margin-bottom: 20px;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
+
