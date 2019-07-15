1eb70a1c7436b9855cc646a655aae83ed4c9e9ba
ShiniYanada <ysthon@gmail.com>
2019-07-15 10:58:38 +0900

    created()に商品を取得する処理を追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,6 @@ export default {
     },
   },
   created(){
-    console.log('created')
     this.$store.dispatch('product/setProducts')
   }
 }
--- src/views/ProductShow.vue
+++ src/views/ProductShow.vue
@@ -1,13 +1,29 @@
 <template>
     <div id="show">
-
+        <p>{{ item.name }}</p>
+        <router-link to="/">ホームへ戻る</router-link>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'productShow',
-    computed: {
+    data(){
+        return {
+            item: {}
+        }
+    },
+    created(){
+        axios.get(`http://localhost:3000/api/v1/products/${this.$route.params.id}`)
+            .then( response => {
+                console.log(response)
+                this.item = response.data
+            })
+            .catch( error => {
+                console.log('error')
+            })
     }
 }
 </script>
