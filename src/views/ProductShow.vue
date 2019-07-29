fd9ea5f43591d4d692d75a7e4c4aa3de2897e213
ShiniYanada <ysthon@gmail.com>
2019-07-29 21:53:02 +0900

    いいね機能を追加

--- src/views/ProductShow.vue
+++ src/views/ProductShow.vue
@@ -4,6 +4,9 @@
         <router-link to="/">ホームへ戻る</router-link><br/>
         <router-link :to="`/sell/${this.$route.params.id}/edit`" v-if="seller">商品の編集</router-link>
         <el-button type="success" round @click="trade" v-if="!seller && open">購入手続き</el-button>
+        <el-button plain @click="like" v-if="!liked">いいね</el-button>
+        <el-button plain @click="unlike" v-if="liked">いいね取り消し</el-button>
+        <el-button circle>{{ item.likes_count }}</el-button>
     </div>
 </template>
 
@@ -14,7 +17,8 @@ export default {
     name: 'productShow',
     data(){
         return {
-            item: {}
+            item: {},
+            liked: false
         }
     },
     computed: {
@@ -28,10 +32,11 @@ export default {
     },
     created(){
         //商品情報を取得
-        request.get(`/api/v1/products/${this.$route.params.id}`, {})
+        request.get(`/api/v1/products/${this.$route.params.id}`, {auth: true})
             .then( response => {
                 console.log(response)
-                this.item = response.data
+                this.item = response.data.product
+                this.liked = response.data.like
             })
             .catch( error => {
                 console.log('error')
@@ -42,6 +47,28 @@ export default {
             request.post(`/api/v1/products/${this.item.id}/trade`, { auth: true })
                 .then( response => console.log("success"))
                 .catch( errors => console.log("error"))
+        },
+        like(){
+            request.post(`/api/v1/products/${this.item.id}/likes`, { auth: true })
+                .then(response => {
+                    console.log(response.data)
+                    this.item.likes_count = response.data.likes_count
+                    this.liked = true
+                })
+                .catch(error => {
+                    console.log('error')
+                })
+        },
+        unlike(){
+            request.delete(`/api/v1/products/${this.item.id}/likes`, { auth: true })
+                .then( response => {
+                    console.log(response.data)
+                    this.item.likes_count = response.data.likes_count
+                    this.liked = false
+                })
+                .catch(error => {
+                    console.log('error')
+                })
         }
     }
 }
