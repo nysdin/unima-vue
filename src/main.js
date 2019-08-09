96605a7f085ad5ab08217719f76b99f042a820d6
ShiniYanada <ysthon@gmail.com>
2019-08-09 11:30:14 +0900

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store/'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -12,5 +13,6 @@ Vue.use(ElementUI)
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
