da916163be26c9393ce3af90b22f57aa354431a7
ShiniYanada <ysthon@gmail.com>
2019-08-28 10:34:42 +0900

    fix

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -25,13 +25,9 @@ const user = {
     },
     mutations: {
         setUser(state, userData){
-            console.log(userData, 'userData')
             const { name, email, provider, uid, id, avatar } = userData
-            console.log(name, email, provider, uid, id, avatar)
             state.user = { name, email, provider, uid, id, avatar }
-            console.log(state.user)
             state.user.avatar.url = avatar.url
-            console.log(state.user.avatar.url)
         },
         removeUser(state){
             const user = { name: '', email: '', provider: '', uid: '', id: 0 }
