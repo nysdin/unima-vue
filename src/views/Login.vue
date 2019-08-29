550abcbfb25db79a88e36836e2bac4e44c60b074
ShiniYanada <ysthon@gmail.com>
2019-08-29 16:03:26 +0900

    ログインボタンを押した際にボタンをローディング仕様に変更

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,7 +8,10 @@
                 label="パスワード" required :type="show ? 'text' : 'password'"
                 @click:append="show = !show"></v-text-field>
 
-                <v-btn medium outlined @click="login">ログイン</v-btn>
+                <v-btn medium outlined @click="login" 
+                    :loading="loading" :disabled="loading">
+                    ログイン
+                </v-btn>
                 <v-btn small text color="primary" class="ml-2"
                     @click="$router.push('/password_reset')">
                     パスワードを忘れた方はこちら
@@ -27,15 +30,19 @@ export default {
         return {
             show: false,
             user: { email: 'ysthon@gmail.com', password: 'password'},
+            loading: false,
         }
     },
     methods: {
         login(){
+            this.loading = true
             this.$store.dispatch('auth/login', this.user)
                 .then( () => {
+                    this.loading =false
                     this.$router.push('/')
                 })
                 .catch( errors => {
+                    this.loading =false
                     errors.forEach(error => {
                         setTimeout( () => {
                             this.renderError(error)
