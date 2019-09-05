936e54c5e5d565d9ae57f7050ce66105c87a5a87
ShiniYanada <ysthon@gmail.com>
2019-09-05 15:09:07 +0900

    一覧に支払い方法を追加

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -49,6 +49,11 @@
                         <v-list-item-title v-text="'銀行口座の登録'"></v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
+                <v-list-item @click="$router.push('/settings/pay')">
+                    <v-list-item-content>
+                        <v-list-item-title v-text="'お支払い方法'"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
                 <v-list-item @click="logout">
                     <v-list-item-content>
                         <v-list-item-title v-text="'ログアウト'"></v-list-item-title>
