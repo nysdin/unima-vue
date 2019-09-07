956773108a2cccc61e18784ca58e18efd211d873
ShiniYanada <ysthon@gmail.com>
2019-09-07 14:48:25 +0900

    クレジットカード情報を登録する画面と処理を追加

---
+++ src/views/CardCreate.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+        <v-container>
+            <v-card :elevation="1">
+                <v-container>
+                    <form id="payment-form">
+                        <div class="form-row mb-2">
+                            <label for="card-element">
+                                クレジットカード
+                            </label>
+                            <div id="card-element">
+                            <!-- A Stripe Element will be inserted here. -->
+                            </div>
+
+                            <!-- Used to display form errors. -->
+                            <div id="card-errors" role="alert"></div>
+                        </div>
+
+                        <div class="d-flex justify-center">
+                            <v-btn outlined @click.stop.prevent="registerCregitCard">登録する</v-btn>
+                        </div>
+                    </form>
+                </v-container>
+            </v-card>
+            
+        </v-container>
+    </div>
+</template>
+
+<script>
+const stripe = Stripe('pk_test_GRAL4nR6T1zOJHJIN9YAwGsI003jSsSSud')
+import request from '../utils/api.js'
+
+const elements = stripe.elements()
+
+const style = {
+    base: {
+        // Add your base input styles here. For example:
+        fontSize: '16px',
+        color: "#32325d",
+    },
+}
+
+const card = elements.create('card', {style, hidePostalCode: true})
+
+export default {
+    name: 'CardCreate',
+    methods: {
+        async registerCregitCard(){
+            const {token, error} = await stripe.createToken(card)
+            if (error) {
+                const errorElement = document.getElementById('card-errors');
+                errorElement.textContent = error.message;
+            } else {
+                request.patch('/api/v1/card', {params: { stripe_cregit_token: token.id }, auth: true})
+                    .then(response => {
+                        console.log(response)
+                        this.$router.push('/settings/card')
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            }
+        }
+    },
+    mounted(){
+        card.mount('#card-element')
+        card.addEventListener('change', ({error}) => {
+            const displayError = document.getElementById('card-errors')
+            if (error) {
+                displayError.textContent = error.message
+            } else {
+                displayError.textContent = ''
+            }
+        })
+    }
+}
+</script>
+
+<style>
+.StripeElement {
+    box-sizing: border-box;
+
+    height: 40px;
+
+    padding: 10px 12px;
+
+    border: 1px solid transparent;
+    border-radius: 4px;
+    background-color: white;
+
+    box-shadow: 0 1px 3px 0 #e6ebf1;
+    -webkit-transition: box-shadow 150ms ease;
+    transition: box-shadow 150ms ease;
+}
+
+.StripeElement--focus {
+    box-shadow: 0 1px 3px 0 #cfd7df;
+}
+
+.StripeElement--invalid {
+    border-color: #fa755a;
+}
+
+.StripeElement--webkit-autofill {
+    background-color: #fefde5 !important;
+}
+</style>
