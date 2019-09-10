d5b016f9b31e6349edc4cf299034f6fde2212ae4
ShiniYanada <ysthon@gmail.com>
2019-09-10 18:15:34 +0900

    import registerServiceWorker

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/'
+import './registerServiceWorker'
 import ElementUI from 'element-ui'
 import Croppa from 'vue-croppa'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
 import 'vue-croppa/dist/vue-croppa.css'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
