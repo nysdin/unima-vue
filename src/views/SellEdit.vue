7a9a7cd7464a60735e783296c1931721c4c4f4ef
ShiniYanada <ysthon@gmail.com>
2019-09-13 14:36:40 +0900

    バリデーションを追加

--- src/views/SellEdit.vue
+++ src/views/SellEdit.vue
@@ -2,34 +2,71 @@
     <div id="sell">
         <h1>商品の編集</h1>
         <v-container>
-            <v-form>
-                <v-row>
-                    <v-col :cols="3" v-for="(image, i) in images" :key="i">
-                        <croppa v-model="images[i]" :width="eachSize" :height="eachSize" ></croppa>
-                    </v-col>
-                </v-row>
-                <v-text-field v-model="product.name" label="商品名" placeholder="商品名" required></v-text-field>
-                <v-textarea label="商品の説明" v-model="product.description"></v-textarea>
-                <v-text-field v-model="product.price" label="商品の価格" placeholder="価格" required></v-text-field>
-                <v-select :items="categoris" v-model="product.category.name" label="カテゴリー"
-                ></v-select>
-                <v-select :items="states" v-model="product.state" label="商品の状態"
-                ></v-select>
+            <ValidationObserver v-slot="{ invalid }">
+                <v-form>
+                    <v-row>
+                        <v-col :cols="3" v-for="(image, i) in images" :key="i">
+                            <croppa v-model="images[i]" :width="eachSize" :height="eachSize" ></croppa>
+                        </v-col>
+                    </v-row>
+                    <ValidationProvider rules="required|max:40" v-slot="{ errors, valid }">
+                        <v-text-field v-model="product.name" label="商品名"
+                            placeholder="商品名" required counter="40"
+                            :error-messages="errors" :success="valid">
+                        </v-text-field>
+                    </ValidationProvider>
+                    <ValidationProvider rules="required|max:1000" v-slot="{ errors, valid }">
+                        <v-textarea label="商品の説明" v-model="product.description"
+                            :error-messages="errors" :success="valid"
+                            required counter="1000">
+                        </v-textarea>
+                    </ValidationProvider>
+                    <ValidationProvider rules="required|price" v-slot="{ errors, valid }">
+                        <v-text-field v-model="product.price" label="商品の価格(~300,000)"
+                            placeholder="価格" required prefix="¥" type="number"
+                            :error-messages="errors" :success="valid">
+                        </v-text-field>
+                    </ValidationProvider>
+                    <ValidationProvider rules="required" v-slot="{ errors, valid }">
+                        <v-select :items="categoris" v-model="product.category.name"
+                            label="カテゴリー" required
+                            :error-messages="errors" :success="valid">
+                        </v-select>
+                    </ValidationProvider>
+                    <ValidationProvider rules="required" v-slot="{ errors, valid }">
+                        <v-select :items="states" v-model="product.state"
+                            label="商品の状態" required
+                            :error-messages="errors" :success="valid">
+                        </v-select>
+                    </ValidationProvider>
 
-                <v-btn medium outlined @click="update">編集する</v-btn>
-                <v-btn medium outlined @click="destroy">削除する</v-btn>
-            </v-form>
+                    <v-btn medium outlined @click="update" 
+                        :disabled="invalid || loading" color="primary">
+                        編集する
+                    </v-btn>
+                    <v-btn medium outlined @click="destroy"
+                        :disabled="loading" color="primary" class="ml-2">
+                        削除する
+                    </v-btn>
+                </v-form>
+            </ValidationObserver>
         </v-container>
     </div>
 </template>
 
 <script>
 import request from '../utils/api.js'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
 
 export default {
     name: 'sell',
+    components: {
+        ValidationProvider,
+        ValidationObserver,
+    },
     data(){
         return{
+            loading: false,
             size: window.innerWidth,
             product: {
                 category: {
@@ -49,20 +86,26 @@ export default {
     },
     methods: {
         update(){
+            this.loading = true
             request.patch(`/api/v1/products/${this.$route.params.id}`, { params: this.product, auth: true})
                 .then( response => {
+                    this.loading = false
                     this.$router.push({ path: `/product/${response.data.id}` })
                 })
                 .catch( error => {
+                    this.loading = false
                     console.log('error')
                 })
         },
         destroy(){
+            this.loading = true
             request.delete(`/api/v1/products/${this.$route.params.id}`, { params: this.product, auth: true })
                 .then( () => {
+                    this.loading = false
                     this.$router.push({ path: '/' })
                 })
                 .catch( () => {
+                    this.loading = false
                     console.log('error')
                 })
         },
