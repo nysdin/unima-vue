bc30bab609b20ab29424462cb50e278601e66e93
ShiniYanada <ysthon@gmail.com>
2019-09-13 15:17:10 +0900

    バリデーションを追加

--- src/views/PasswordReset.vue
+++ src/views/PasswordReset.vue
@@ -1,42 +1,67 @@
 <template>
     <div id="password-reset">
-        <h1 class="title text-center">パスワード変更用メールアドレス</h1>
-        
         <v-container>
-            <v-form ref="forms">
-                <v-text-field v-model="email" 
-                    label="メールアドレス" @click:append="show1 = !show1">
-                </v-text-field>
-
-                <div class="d-flex justify-center">
-                    <v-btn text large outlined @click="sendEmail">送信する</v-btn>
-                </div>
-            </v-form>
+            <v-card flat>
+                <v-card-title>
+                    <span class="subtitle-1">パスワード変更用メールアドレス</span>
+                </v-card-title>
+                <v-card-text>
+                    <ValidationObserver v-slot="{ invalid }">
+                        <v-form ref="forms">
+                            <ValidationProvider name="ニックネーム" rules="required|email" v-slot="{ errors, valid }">
+                                <v-text-field v-model="email" 
+                                    label="メールアドレス" @click:append="show1 = !show1"
+                                    :success="valid" :error-messages="errors">
+                                </v-text-field>
+                            </ValidationProvider>
+                            <div class="d-flex justify-center">
+                                <v-btn medium outlined 
+                                    color="primary" @click="sendEmail"
+                                    :loading="loading" :disabled="loading || invalid">
+                                    送信する
+                                </v-btn>
+                            </div>
+                        </v-form>
+                    </ValidationObserver>
+                </v-card-text>
+            </v-card>
         </v-container>
-
     </div>
 </template>
 
 <script>
 import request from '../utils/api.js'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
 
 export default {
     name: 'passwordReset',
+    components: {
+        ValidationProvider,
+        ValidationObserver,
+    },
     data(){
         return {
-            email: ''
+            email: '',
+            loading: false,
         }
     },
     methods: {
         sendEmail(){
+            this.loading = true
             request.post('/api/v1/auth/password', {
                 params: {
                     email: this.email,
                     redirect_url: process.env.VUE_APP_CLIENT_URL + '/password/edit'
                 }
             })
-            .then( () => console.log('success'))
-            .catch( () => console.log('failed'))
+            .then( () => {
+                this.loading = false
+                console.log('success')
+            })
+            .catch( () => {
+                this.loading = false
+                console.log('failed')
+            })
         },
     }
 }
