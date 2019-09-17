a3ce3816d8ed959424e221c4377e8eb7394f3e8c
ShiniYanada <ysthon@gmail.com>
2019-09-17 16:32:26 +0900

    不要なelement-uiを削除

--- src/main.js
+++ src/main.js
@@ -2,18 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/'
-import './registerServiceWorker'
-import ElementUI from 'element-ui'
 import Croppa from 'vue-croppa'
+import vuetify from './plugins/vuetify'
+import './registerServiceWorker'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'vue-croppa/dist/vue-croppa.css'
-import vuetify from './plugins/vuetify'
 import './vee-validate'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$stripe = Stripe(process.env.VUE_APP_STRIPE_PUBLIC_API_KEY)
-Vue.use(ElementUI)
+
 Vue.use(Croppa)
 
 new Vue({
